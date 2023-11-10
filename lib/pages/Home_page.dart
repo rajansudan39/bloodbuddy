055b4d0c1198f7b3c775050a9447539500ca7902
Anas Ansari <77924312+anas-ansri@users.noreply.github.com>
2023-11-10 14:02:03 +0530

    update Home_page.dart

--- lib/pages/Home_page.dart
+++ lib/pages/Home_page.dart
@@ -18,6 +18,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final user = FirebaseAuth.instance.currentUser!;
 
+
   //sign user out
   void signUserOut() {
     FirebaseAuth.instance.signOut();
