1bab54f3499f63ba019d4361eea51233bd305134
Anas Ansari <77924312+anas-ansri@users.noreply.github.com>
2023-11-10 16:08:26 +0530

    update auth_page.dart

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:modernlogintute/pages/home_page.dart';
-
 import 'login_or_registe.dart';
 
 class AuthPage extends StatelessWidget {
@@ -15,12 +14,11 @@ class AuthPage extends StatelessWidget {
         builder: (context, snapshot) {
           //user logged in
           if (snapshot.hasData) {
-            return HomePage();
+            return const HomePage();
           }
           //user isnot logged in
-
           else {
-            return LoginOrRegisterPage();
+            return const LoginOrRegisterPage();
           }
         },
       ),
